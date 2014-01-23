7803823b56a6eebff2648388171a9f4197bfbbc1
hands-rec <hands.records@gmail.com>
2014-01-24 02:49:47 +0900

    P77

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -9,12 +9,16 @@ module Codebreaker
       @output.puts 'Enter guess:'
     end
     def guess(guess)
-      if exact_match?(guess, 0)
-        mark = '+'
-      elsif number_match?(guess, 0)
-        mark = '-'
-      else
-        mark = ''
+      mark = ''
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          mark << '+'
+        end
+      end
+      (0..3).each do |index|
+        if number_match?(guess, index)
+          mark << '-'
+        end
       end
       @output.puts mark
     end
@@ -24,7 +28,7 @@ module Codebreaker
     end
 
     def number_match?(guess, index)
-      @secret.include?(guess[index])
+      @secret.include?(guess[index]) && !exact_match?(guess, index)
     end
   end
 end
