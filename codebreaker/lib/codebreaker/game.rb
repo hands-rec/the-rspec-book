29c8e0590f3de4329f5ee542910f4c2b5a239d16
hands-rec <hands.records@gmail.com>
2014-01-24 21:54:18 +0900

    ch 8.2

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -9,26 +9,8 @@ module Codebreaker
       @output.puts 'Enter guess:'
     end
     def guess(guess)
-      mark = ''
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          mark << '+'
-        end
-      end
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          mark << '-'
-        end
-      end
-      @output.puts mark
-    end
-
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
-    end
-
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
+      maker = Maker.new(@secret,guess)
+      @output.puts '+'*maker.exact_match_count + '-'*maker.number_match_count
     end
   end
 end
